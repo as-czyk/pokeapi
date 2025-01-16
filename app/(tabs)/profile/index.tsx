38ae9bbf18d675f8065dea9097200055c40d8a16
asczyk <aron.scheffczyk@gmail.com>
2025-01-16 09:28:52 +0100

    add logout; clean imports

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,5 +1,24 @@
-import { Text } from "react-native";
+import { CloudApi } from "@/cloud/CloudApi";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity } from "react-native";
 
 export default function ProfilePage() {
-  return <Text>Prpofile</Text>;
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      await CloudApi.logout();
+      router.push("/");
+    } catch (e) {
+      console.log("Logout failed:", e);
+    }
+  };
+
+  return (
+    <TouchableOpacity
+      className="bg-green-500 rounded-full py-3 items-center"
+      onPress={handleLogout}
+    >
+      <Text className="text-white font-bold text-lg">Logout</Text>
+    </TouchableOpacity>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,6 @@
-import { useColorScheme } from "@/hooks/useColorScheme";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Slot, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
@@ -50,7 +44,10 @@ export default function RootLayout() {
     <SafeAreaProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(auth)/login"
+          options={{ headerBackTitle: "Back", title: "Login" }}
+        />
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
     </SafeAreaProvider>
